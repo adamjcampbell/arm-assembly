0ca7f926569c1c1a7bc51e70853a280651adfb98
Adam Campbell <adam.92.ac@gmail.com>
2024-09-22 16:28:56 +1000

    Extract display prompt and exit

--- shell.s
+++ shell.s
@@ -1,17 +1,23 @@
 .section .data
 prompt:
-    .asciz "$ "         @ The shell prompt $
+    .asciz "$ "         @ The shell prompt $ with a trailing space
 
 .section .text
 .global _start
 
 _start:
+    b display_prompt    @ Display the shell prompt
+
+display_prompt:
     ldr r0, =prompt     @ Load address of the message into r0
     mov r1, r0          @ Set r1 to point to the message
-    mov r2, #2          @ Set r2 to the length of the message
+    mov r2, #2          @ Set r2 to the length of prompt
     mov r7, #4          @ System call number for sys_write
     mov r0, #1          @ File descriptor 1 (standard output)
     svc 0               @ Make the system call
+    b exit
+
+exit:
     mov r7, #1          @ System call number for sys_exit
     mov r0, #0          @ Exit code 0
     svc 0               @ Make the system call to exit
